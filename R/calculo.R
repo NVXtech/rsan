7a13e1d695b1c660cb77cef415d33b69c4537f29
Julio Werner <juliowernery@gmail.com>
2025-03-27 08:56:13 -0300

    add log message

--- R/calculo.R
+++ R/calculo.R
@@ -88,6 +88,7 @@ investimento_drenagem <- function(state) {
   tabela <- investimento_cadastro(tabela, input$custo_cadastro)
   tabela <- investimento_total_drenagem(tabela)
   tabela <- adiciona_pais(tabela)
+  rlog::info("drenagem: removendo não críticos")
   tabela <- remove_nao_criticos(tabela)
   state$drenagem <- tabela
   state$necessidade <- dplyr::bind_rows(
