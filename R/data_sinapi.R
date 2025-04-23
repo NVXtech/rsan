185c56977d48e9f96217f98dee14413398b9818d
Julio Werner <juliowernery@gmail.com>
2025-04-23 13:56:40 -0300

    feat: implement download and processing functions for SINAPI 2025 data; update directory paths for data storage

--- R/data_sinapi.R
+++ R/data_sinapi.R
@@ -1,4 +1,6 @@
-sinapi_base_path <- file.path("dados", "brutos", "sinapi")
+sinapi_dir_bruto <- file.path("dados", "brutos", "sinapi")
+sinapi_dir_base <- file.path("dados", "base_calculo")
+
 #' Último ano e mês
 #'
 #' @return uma `list` com o último ano e mes
@@ -43,10 +45,10 @@ download_sinapi <- function(year, month) {
     rlog::log_info(sprintf("Baixando SINAPI do ESTADO %s", state))
     rlog::log_info(sprintf("SINAPI URL: %s", url))
     tmp_file <- paste0("sinapi", year, month, state, ".zip")
-    path_out <- file.path(sinapi_base_path, tmp_file)
+    path_out <- file.path(sinapi_dir_bruto, tmp_file)
     tmp_dir <- tempdir()
-    if (!dir.exists(sinapi_base_path)) {
-      dir.create(sinapi_base_path, recursive = TRUE)
+    if (!dir.exists(sinapi_dir_bruto)) {
+      dir.create(sinapi_dir_bruto, recursive = TRUE)
     }
     if (file.exists(path_out)) {
       rlog::log_info(sprintf("Arquivo %s já existe", path_out))
@@ -81,7 +83,7 @@ download_sinapi <- function(year, month) {
           "CODIGO", "DESCRICAO DO INSUMO",
           "UNIDADE DE MEDIDA", "PRECO MEDIANO R$"
         )
-        yearmonth <- paste0(year, month)
+        # yearmonth <- paste0(year, month)
         # if (as.integer(yearmonth) >= 202206) {
         #  colunas[3] <- "UNIDADE"
         # }
@@ -211,4 +213,113 @@ sinapi_id_to_name <- function(id) {
 #' @export
 load_sinapi <- function(id) {
   load_data("sinapi")[[id]]
+}
+
+
+#' Download SINAPI 2025
+#' Salva dados brutos do sinapi
+#'
+#' @param ano um `number` com o ano desejado
+#' @param mes um `number` com o mês desejado
+#'
+#' @return filename do arquivo baixado
+#' @export
+download_sinapi_v2025 <- function(ano, mes) {
+  h <- curl::new_handle()
+  caixa_url <- "https://www.caixa.gov.br/site/Paginas/downloads.aspx"
+  req <- curl::curl_fetch_memory(caixa_url, handle = h)
+  curl::handle_cookies(h)
+  download_url <- sprintf(
+    "https://www.caixa.gov.br/poder-publico/modernizacao-gestao/sinapi/Documents/relatorios-mensais/%04d/SINAPI-%04d-%02d-formato-xlsx.zip",
+    ano,
+    ano,
+    mes
+  )
+  zip_name <- sprintf("sinapi%04d%02d.zip", ano, mes)
+  path_out <- file.path(sinapi_dir_bruto, zip_name)
+  if (file.exists(path_out)) {
+    rlog::log_info(sprintf("Arquivo %s já existe", path_out))
+  } else {
+    req <- curl::curl_fetch_disk(download_url, path_out, handle = h)
+    if (req$status_code != 200) {
+      rlog::log_info(sprintf("Erro ao baixar o arquivo %s", download_url))
+      stop("Erro ao baixar o arquivo")
+    }
+  }
+  arquivo <- sprintf("SINAPI_Referência_%04d_%02d.xlsx", ano, mes)
+  caminho_xlsx <- file.path(sinapi_dir_bruto, arquivo)
+  if (!file.exists(caminho_xlsx)) {
+    utils::unzip(path_out,
+      exdir = sinapi_dir_bruto,
+      files = c(arquivo),
+      unzip = "unzip"
+    )
+  }
+  return(file.path(sinapi_dir_bruto, arquivo))
+}
+
+#' SINAPI Formato 2025
+#' Ingestao de Insumos e Composições Não Desonerados (Sem Desoneração)
+#'
+#' @param ano um `number` com o ano desejado
+#' @param mes um `number` com o mês desejado
+#'
+#' @export
+processa_sinapi_v2025 <- function(ano, mes) {
+  caminho_xlsx <- download_sinapi_v2025(ano, mes)
+  insumos <- readxl::read_excel(caminho_xlsx, sheet = "ISD", skip = 9, col_types = "text")
+  col_names <- c("CLASSIFICACAO", "CODIGO", "DESCRICAO", "UNIDADE", "ORIGEM_PRECO", names(insumos)[6:length(insumos)])
+  names(insumos) <- col_names
+  insumos <- dplyr::select(insumos, -c("CLASSIFICACAO", "ORIGEM_PRECO"))
+  col_names <- names(insumos)
+  for (i in 4:length(col_names)) {
+    col_names[i] <- paste0("PRECO_", col_names[i])
+  }
+  names(insumos) <- col_names
+  composicoes <- readxl::read_excel(
+    caminho_xlsx,
+    sheet = "CSD",
+    skip = 8,
+    col_types = "text"
+  )
+  col_names <- names(composicoes)
+  # remove all cols names that begin with "..."
+  col_names <- col_names[!grepl("^\\.{3}", col_names)]
+  new_col_names <- c("GRUPO", "CODIGO", "DESCRICAO", "UNIDADE")
+  for (col in col_names) {
+    new_col_names <- c(new_col_names, paste0("PRECO_", col), paste0("AS_", col))
+  }
+  names(composicoes) <- new_col_names
+  composicoes <- dplyr::select(composicoes, -c("GRUPO"))
+  composicoes <- dplyr::select(composicoes, -dplyr::starts_with("AS_"))
+
+  df <- dplyr::bind_rows(insumos, composicoes)
+  arquivo_saida <- sprintf("sinapi_%04d%02d.csv", ano, mes)
+  caminho <- file.path(sinapi_dir_base, arquivo_saida)
+  readr::write_excel_csv2(df, caminho, quote = "needed", append = FALSE)
+}
+
+#' Lista de Dados SINAPI disponiveis
+#'
+#' @return lista de dados sinapi
+lista_sinapi <- function() {
+  pattern <- "^sinapi_"
+  sinapi_list <- tools::file_path_sans_ext(
+    list.files(sinapi_dir_base, pattern)
+  )
+  return(sinapi_list)
+}
+
+#' Carrega dados do SINAPI
+#'
+#' @param nome Nome do conjunto de dados retornado pelo lista_sinapi
+#'
+#' @return um `data.frame` com os dados do sinapi
+carrega_sinapi <- function(nome) {
+  arquivo <- file.path(
+    sinapi_dir_base,
+    paste0(nome, ".csv")
+  )
+  sinapi <- readr::read_csv2(arquivo, col_types = "c")
+  return(sinapi)
 }
